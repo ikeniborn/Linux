62f69efc7ad72f5a40930efdcf52f2e61b34491c
ikeniborn <ikeniborn@gmail.com>
2020-06-30 16:36:17 +0300

    up

--- Bash/backup.sh
+++ Bash/backup.sh
@@ -9,43 +9,86 @@ else
 fi
 
 #Parametrs
-BACKUPTIME=`date +'%d-%b-%Y %R'`
-PCT='/pictures'
-DT='/data'
-VM='/VirtualBox VMs'
 SRCPCT='/media/pictures'
 SRCDT='/media/data'
-SCRVM='/home/ikeni/VirtualBox VMs'
+SCRVM='/media/ikeni/Virtual_HDD'
+SCRGD='/home/ikeni/google-drive/ПК'
 TGT='/mnt/bacula'
-TGTPCT=$TGT$PCT
-TGTDT=$TGT$DT
-TGTVM=$TGT$VM
+TGTMEDIA=$TGT'/media'
+TGTHOME=$TGT'/home'
+TGTHOMEIKENI=$TGTHOME'/ikeni'
+TGTMEDIAIKENI=$TGTMEDIA'/ikeni'
+TGTHOMEIKENIGOOGLEDRIVE=$TGTHOMEIKENI'/google-drive'
 
 #Backup data
 OLD_IFS=$IFS
 IFS=$'\n'
 echo  &>>$LOGFILE
 echo '====================================================='  &>>$LOGFILE
-echo $BACKUPTIME &>>$LOGFILE
-echo 'Задание запущено...' &>>$LOGFILE
+echo 'Задание запущено в '`date +'%d-%b-%Y %R'` &>>$LOGFILE
+echo '====================================================='  &>>$LOGFILE
 
-if [[ $DATE = 1 ]]; then
-  echo 'Удаление старого архива '$TGTDT &>>$LOGFILE;
-  rm -rf $TGTDT &>>$LOGFILE;
-  echo 'Удаление старого архива '$TGTPCT &>>$LOGFILE;
-  rm -rf $TGTPCT &>>$LOGFILE;
-  echo 'Удаление старого архива '$TGTVM &>>$LOGFILE;
-  rm -rf $TGTVM &>>$LOGFILE;
-  echo 'Создание копии...' >>$LOGFILE
-  rsync -azrqW --exclude '*/.Trash*' $SRCDT $TGT & rsync -azrqW --exclude '*/.Trash*' $SRCPCT $TGT \
-  & rsync -azrqW --exclude '*/.Trash*' $SCRVM $TGT;
-  echo 'Копии созданы' >>$LOGFILE
-else
-  echo 'Обновление...' >>$LOGFILE
-  rsync -azrqW --exclude '*/.Trash*' $SRCDT $TGT & rsync -azrqW --exclude '*/.Trash*' $SRCPCT $TGT \
-  & rsync -azrqW --exclude '*/.Trash*' $SCRVM $TGT;
-  echo 'Копии обновлены' >>$LOGFILE
+echo 'Проверка каталогов' >>$LOGFILE
+
+if ! [ -d $TGTMEDIA ]; then
+mkdir -p $TGTMEDIA 
+echo $TGTMEDIA' cоздан' >>$LOGFILE
+  else 
+  echo $TGTMEDIA' существует' >>$LOGFILE
 fi
+
+if ! [ -d $TGTHOME ]; then
+  mkdir -p $TGTHOME 
+  echo $TGTHOME' cоздан' >>$LOGFILE
+else 
+echo $TGTHOME' существует' >>$LOGFILE
+fi
+
+if ! [ -d $TGTMEDIAIKENI ]; then
+mkdir -p $TGTMEDIAIKENI 
+echo $TGTMEDIAIKENI ' cоздан' >>$LOGFILE
+else 
+echo $TGTMEDIAIKENI ' существует' >>$LOGFILE
+fi
+
+if ! [ -d $TGTHOMEIKENI ]; then
+mkdir -p $TGTHOMEIKENI 
+echo $TGTHOMEIKENI ' cоздан' >>$LOGFILE
+else 
+echo $TGTHOMEIKENI ' существует' >>$LOGFILE
+fi
+
+if ! [ -d $TGTHOMEIKENIGOOGLEDRIVE ]; then
+mkdir -p $TGTHOMEIKENIGOOGLEDRIVE 
+echo $TGTHOMEIKENIGOOGLEDRIVE ' cоздан' >>$LOGFILE
+else 
+echo $TGTHOMEIKENIGOOGLEDRIVE' существует' >>$LOGFILE
+fi
+echo 'Каталоги проверены' >>$LOGFILE
+
+echo 'Обновление...' >>$LOGFILE
+
+echo 'Обновление ' $TGT$SRCDT >>$LOGFILE
+echo 'Запуск ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+rsync -azrqW --exclude '*/.Trash*' $SRCDT $TGTMEDIA;
+echo 'Завершение ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+
+echo 'Обновление ' $TGT$SRCPCT >>$LOGFILE
+echo 'Запуск ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+rsync -azrqW --exclude '*/.Trash*' $SRCPCT $TGTMEDIA; 
+echo 'Завершение ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+
+echo 'Обновление ' $TGT$SCRGD >>$LOGFILE
+echo 'Запуск ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+rsync -azrqW --exclude '*/.Trash*' $SCRGD $TGTHOMEIKENIGOOGLEDRIVE;
+echo 'Завершение ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+
+echo 'Обновление ' $TGT$SCRVM >>$LOGFILE
+echo 'Запуск ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+ls -p $SCRVM | grep -v / | xargs -I '{}' -P 2 -n1 rsync -azrqW --exclude '*/.Trash*' $SCRVM $TGTMEDIAIKENI;
+echo 'Завершение ' `date +'%d-%b-%Y %R'` >>$LOGFILE
+
+echo 'Копии обновлены' >>$LOGFILE
 
 STATUS=$?
 IFS=$OLD_IFS
@@ -56,11 +99,15 @@ if [[ $STATUS != 0 ]]; then
   echo '###########################################' &>>$LOGFILE;
 else
   echo 'Размер исходного каталога '$SRCDT' - ' `du -h -s --exclude '*/.Trash*' $SRCDT | awk '{print $1}'` &>> $LOGFILE
-  echo 'Размер копии '$TGTDT' - ' `du -h -s --exclude '*/.Trash*' $TGTDT | awk '{print $1}'` &>>$LOGFILE
+  echo 'Размер копии '$TGT$SRCDT' - ' `du -h -s --exclude '*/.Trash*' $TGT$SRCDT | awk '{print $1}'` &>>$LOGFILE
   echo 'Размер исходного каталога '$SRCPCT' - ' `du -h -s --exclude '*/.Trash*' $SRCPCT | awk '{print $1}'` &>> $LOGFILE
-  echo 'Размер копии '$TGTPCT' - ' `du -h -s --exclude '*/.Trash*' $TGTPCT | awk '{print $1}'`  &>>$LOGFILE
+  echo 'Размер копии '$TGT$SRCPCT' - ' `du -h -s --exclude '*/.Trash*' $TGT$SRCPCT | awk '{print $1}'`  &>>$LOGFILE
   echo 'Размер исходного каталога '$SCRVM' - ' `du -h -s --exclude '*/.Trash*' $SCRVM | awk '{print $1}'` &>> $LOGFILE
-  echo 'Размер копии '$TGTVM' - ' `du -h -s --exclude '*/.Trash*' $TGTVM | awk '{print $1}'`  &>>$LOGFILE
+  echo 'Размер копии '$TGT$SCRVM' - ' `du -h -s --exclude '*/.Trash*' $TGT$SCRVM | awk '{print $1}'`  &>>$LOGFILE
+  echo 'Размер исходного каталога '$SCRGD' - ' `du -h -s --exclude '*/.Trash*' $SCRGD | awk '{print $1}'` &>> $LOGFILE
+  echo 'Размер копии '$TGT$SCRGD' - ' `du -h -s --exclude '*/.Trash*' $TGT$SCRGD | awk '{print $1}'`  &>>$LOGFILE
+  echo '====================================================='  &>>$LOGFILE
   echo 'Бэкап успешно завершен в '`date +'%d-%b-%Y %R'` &>>$LOGFILE;
+  echo '====================================================='  &>>$LOGFILE
 fi
 exit
